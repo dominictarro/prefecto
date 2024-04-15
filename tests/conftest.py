049ccf1cd14d0973d9f3870e2bafdf69f226cbcc
Dominic Tarro <dominic.tarro@gmail.com>
2024-04-14 21:03:15 -0400

    Fix conftest to treat harness as context manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,5 @@ from prefect.testing.utilities import prefect_test_harness
 @pytest.fixture
 def harness():
     """Return a `prefect_test_harness`."""
-    try:
-        prefect_test_harness()
-    finally:
-        pass
+    with prefect_test_harness():
+        yield
