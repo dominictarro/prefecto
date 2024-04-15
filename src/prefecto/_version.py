ed978f89ad1e1c7d8ba2ddfec9be4630c1009967
Dominic Tarro <dominic.tarro@gmail.com>
2024-04-14 20:00:58 -0400

    Make versioneer unvendored

--- src/prefecto/_version.py
+++ src/prefecto/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = ""
     cfg.versionfile_source = "src/prefecto/_version.py"
     cfg.verbose = False
